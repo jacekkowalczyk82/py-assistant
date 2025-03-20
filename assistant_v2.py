042eeae0bd1be3365a3c8fd7dee4f9c657827e43
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2025-03-20 16:22:40 +0100

    assistant loop handling more commands

--- assistant_v2.py
+++ assistant_v2.py
@@ -11,20 +11,28 @@ CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 10
+RECORD_SECONDS = 5
 OUTPUT_FILE = "output.wav"
 
-ASSISTANT_OFF = "assistant off"
+ASSISTANT_OFF = "assistant turn off"
+ASSISTANT_COMMANDS_PREFIX = "assistant "
+SUPPORTED_COMMANDS = ["play radio RMF", "stop radio", "volume up", "volume down", "play youtube favorites", "stop youtube", "help me"]
+
+def assist_print(text):
+    print("\n    " + text)
+def show_assistent_help():
+    assist_print("Supported commands are: " + str(SUPPORTED_COMMANDS))
+
 
 def listen_speech():
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-    print(f"Listening...{RECORD_SECONDS} seconds")
+    assist_print(f"Listening...{RECORD_SECONDS} seconds")
     frames = []
     for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
         data = stream.read(CHUNK)
         frames.append(data)
-    print("Done recording.")
+    assist_print("Done recording.")
 
     stream.stop_stream()
     stream.close()
@@ -53,6 +61,27 @@ def listen_speech():
         return None
 
 
+def is_supported (command_text):
+    if ASSISTANT_OFF  in command_text:
+        return True
+
+    for command in SUPPORTED_COMMANDS:
+        full_command = ASSISTANT_COMMANDS_PREFIX + command
+        if full_command.lower() in command_text.lower():
+            return True
+
+
+    assist_print(command_text + " does not contain any supported command ")
+    show_assistent_help();
+    return False; 
+
+def handle_supported_command(command_text):
+    if is_supported(command_text):
+        assist_print("Received command: " + command_text.lower())
+
+        lower_command = command_text.lower()
+        assist_print("Running command: " + lower_command)
+
 
 if __name__ == "__main__":
     # execute only if run as a script
@@ -61,13 +90,17 @@ if __name__ == "__main__":
     while keep_listening_assitant :
         text = listen_speech() ;
         if text is None:
-            println ("text: None: no speach ") 
-        if ASSISTANT_OFF  in text:
-            keep_listening_assitant = False
+            assist_print ("text: None: no speach ") 
+        else: 
+            handle_supported_command (text)
+
+            if ASSISTANT_OFF  in text:
+                keep_listening_assitant = False
         
         
+
         # waiting for next 
-        time.sleep(10) # 10 seconds 
+        time.sleep(5) #  seconds 
 
         
     
